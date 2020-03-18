f5b1a5adbad11aabf6edda2944ef25e108dd5c6d
youp bos <2107418@talnet.nl>
2020-03-18 13:49:18 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,4 +1,24 @@
--- CREATE DATABASE NetLand;
+-- select title
+-- from series
+-- where has_won_awards >= 1;
 
-SELECT *
-FROM series;
+-- select title
+-- from series
+-- where rating >= 2.5;
+
+-- select title
+-- from series
+-- where language = "EN";
+
+-- select title
+-- from series
+-- where seasons <= 4;
+
+-- select title
+-- from series
+-- where rating = 5;
+
+-- select title
+-- from series
+-- where seasons <=3 and seasons >=20;
+
